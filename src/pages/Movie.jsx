39739b9c92d448569781ef2a4e694456418ba2e6
winniesiriba <winniemoraa234@gmail.com>
2025-04-27 05:55:33 +0300

    finished codegrade

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,14 +1,27 @@
 import { useEffect, useState } from "react";
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+     fetch(`http://localhost:4000/movies/${id}`)
+       .then((r) => r.json())
+       .then(setMovie);
+  }, [id]);
+
+if (!movie) return <div>Loading...</div>;
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
+       <NavBar />
+      <h1>{movie.title}</h1>
+      <p>{movie.time} minutes</p>
+      {movie.genres.map((genre, index) => (
+        <span key={index}>{genre} </span>
+      ))}
+
+  
     </>
   );
 };
